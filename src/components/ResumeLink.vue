9d2f60749c84c2b6012bb293be9ce8d2d5509ddd
staniboy <stanley.v@outlook.com>
2023-10-29 02:18:16 +0400

    feat: link component

---
+++ src/components/ResumeLink.vue
@@ -0,0 +1,9 @@
+<template>
+  <a :href="props.href" class="text-lime-700 font-medium">{{ title }}</a>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  href: string
+  title: string
+}>()
+</script>
