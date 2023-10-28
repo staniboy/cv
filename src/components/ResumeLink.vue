44f0542e1b9ae89cb6cb06a198b52c40d8a8bc96
staniboy <stanley.v@outlook.com>
2023-10-29 02:46:30 +0400

    feat: hover effect

--- src/components/ResumeLink.vue
+++ src/components/ResumeLink.vue
@@ -1,5 +1,7 @@
 <template>
-  <a :href="props.href" class="text-lime-700 font-medium">{{ title }}</a>
+  <a :href="props.href" class="text-green-600 hover:text-green-800 font-medium cursor-pointer">
+    {{ title }}
+  </a>
 </template>
 <script setup lang="ts">
 const props = defineProps<{
