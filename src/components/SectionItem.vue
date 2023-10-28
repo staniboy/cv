22a3403289314cfd81bd552a2fc91d068e4fdac0
staniboy <stanley.v@outlook.com>
2023-10-29 02:43:53 +0400

    fix: spacing

--- src/components/SectionItem.vue
+++ src/components/SectionItem.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <h3 class="text-lg font-medium">{{ props.title }}</h3>
-    <h3 v-if="props.subtitle" class="text-md mb-4 text-slate-500">{{ props.subtitle }}</h3>
-    <slot></slot>
+  <div class="flex flex-col gap-1">
+    <div>
+      <h3 class="text-lg font-medium">{{ props.title }}</h3>
+      <h3 v-if="props.subtitle" class="text-md text-slate-500">{{ props.subtitle }}</h3>
+    </div>
+    <div><slot></slot></div>
   </div>
 </template>
 <script setup lang="ts">
