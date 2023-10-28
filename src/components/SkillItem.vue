0d3462e79cc529b8b4b7e50976dae092ff124026
staniboy <stanley.v@outlook.com>
2023-10-29 03:33:20 +0400

    feat: skill component

---
+++ src/components/SkillItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="text-md pb-1">{{ props.title }}</div>
+    <div class="w-full h-1 bg-slate-200">
+      <div class="h-1 bg-green-600" :style="{ width: `${percent}%` }"></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  percent: number
+}>()
+</script>
